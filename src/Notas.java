19a1af5a55941ad3397c59f384cb5920bb927f7e
Kevin Enrique Coloma Fonseca <kevin.coloma@epn.edu.ec>
2023-11-24 11:31:36 -0500

    Create class "Notas"

---
+++ src/Notas.java
@@ -0,0 +1,59 @@
+public class Notas {
+    //Atributos de cada materia
+    public float notaMateria1;
+    public float notaMAteria2;
+    public float notaMAteria3;
+    public float notaMAteria4;
+    public float notaMAteria5;
+    //Costructor --> Inicializa la clase
+
+    public Notas(float notaMateria1, float notaMAteria2, float notaMAteria3, float notaMAteria4, float notaMAteria5) {
+        this.notaMateria1 = notaMateria1;
+        this.notaMAteria2 = notaMAteria2;
+        this.notaMAteria3 = notaMAteria3;
+        this.notaMAteria4 = notaMAteria4;
+        this.notaMAteria5 = notaMAteria5;
+    }
+    //Getters
+
+    public float getNotaMateria1() {
+        return notaMateria1;
+    }
+
+    public float getNotaMAteria2() {
+        return notaMAteria2;
+    }
+
+    public float getNotaMAteria3() {
+        return notaMAteria3;
+    }
+
+    public float getNotaMAteria4() {
+        return notaMAteria4;
+    }
+
+    public float getNotaMAteria5() {
+        return notaMAteria5;
+    }
+    //Setteres
+
+    public void setNotaMateria1(float notaMateria1) {
+        this.notaMateria1 = notaMateria1;
+    }
+
+    public void setNotaMAteria2(float notaMAteria2) {
+        this.notaMAteria2 = notaMAteria2;
+    }
+
+    public void setNotaMAteria3(float notaMAteria3) {
+        this.notaMAteria3 = notaMAteria3;
+    }
+
+    public void setNotaMAteria4(float notaMAteria4) {
+        this.notaMAteria4 = notaMAteria4;
+    }
+
+    public void setNotaMAteria5(float notaMAteria5) {
+        this.notaMAteria5 = notaMAteria5;
+    }
+}
