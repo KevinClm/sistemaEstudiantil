77dc127b95f2b85f2f3b9e5c9d926fd707de5cf7
Kevin Enrique Coloma Fonseca <kevin.coloma@epn.edu.ec>
2023-11-24 11:55:51 -0500

    Create Class Materias

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Trabajo en clase sistema de estudiantes");
-        String student = "Kevin Coloma";
         Notas kevin = new Notas(7.5, 8, 9, 7, 6.4);
     }
 }
---
+++ src/Materias.java
@@ -0,0 +1,20 @@
+public class Materias {
+    //Atributos
+    public String name;
+    //Costructor
+
+    public Materias(String name) {
+        this.name = name;
+    }
+    //Getters
+
+    public String getName() {
+        return name;
+    }
+
+    //Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
